1e0817478271de97ea9bb79ed7ccecad8a6f274d
jstrzyzykowski <jakub.strzyzykowski@gmail.com>
2020-07-21 01:02:46 +0200

    Add wallet class

---
+++ SlotMachine/Wallet.js
@@ -0,0 +1,24 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+        this.getWalletValue = () => _money;
+        this.checkCanPlay = value => {
+            if (_money >= value) return true;
+            return false;
+        }
+        this.changeWallet = (value, type = "+") => {
+            if (typeof value === "number" && !isNaN(value)) {
+                if (type === "+") return _money += value;
+                else if (type === "-") return _money -= value;
+                else throw new Error('Invalid operation type');
+            } else {
+                console.log(typeof value);
+                throw new Error('Invalid number');
+            }
+        }
+    }
+
+    //Methods
+}
+
+const wallet = new Wallet(200);

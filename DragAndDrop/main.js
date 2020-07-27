9d111a91bf216e5f8a50b43d38e150c1333c4c74
jstrzyzykowski <jakub.strzyzykowski@gmail.com>
2020-07-27 14:31:01 +0200

    Add callElement

--- DragAndDrop/main.js
+++ DragAndDrop/main.js
@@ -1,8 +1,83 @@
 const squareDiv = document.querySelector('.square');
 
+let topDiv = 150;
+let leftDiv = 150;
+
+squareDiv.style.top = `${topDiv}px`;
+squareDiv.style.left = `${leftDiv}px`;
+
 let isActive = false;
 let insertDivX;
 let insertDivY;
+
+const callElement = (e) => {
+    // console.log(`X:${e.clientX} | Y:${e.clientY}`);
+    squareDiv.style.top = `${e.clientY}px`;
+    squareDiv.style.left = `${e.clientX}px`;
+
+    squareDiv.animate([{
+        top: `${e.clientY}px`,
+        left: `${e.clientX}px`,
+        transform: `translate(-50%, -50%)`
+    }], {
+        duration: 100,
+        iterations: 1,
+        fill: "both"
+    });
+
+
+}
+
+const move = (direction) => {
+    let value;
+
+    if (direction === "up") {
+        value = parseInt(squareDiv.style.top.split('').slice(0, -2).join(''));
+        value -= 10;
+        squareDiv.style.top = `${value}px`;
+        console.log(`DIR:UP -> ${value}`);
+    } else if (direction === "down") {
+        value = parseInt(squareDiv.style.top.split('').slice(0, -2).join(''));
+        value += 10;
+        squareDiv.style.top = `${value}px`;
+        console.log(`DIR:DOWN -> ${value}`);
+    } else if (direction === "left") {
+        value = parseInt(squareDiv.style.left.split('').slice(0, -2).join(''));
+        value -= 10;
+        squareDiv.style.left = `${value}px`;
+        console.log(`DIR:LEFT -> ${value}`);
+    } else {
+        value = parseInt(squareDiv.style.left.split('').slice(0, -2).join(''));
+        value += 10;
+        squareDiv.style.left = `${value}px`;
+        console.log(`DIR:RIGHT -> ${value}`);
+    }
+}
+
+const moveElement = (e) => {
+    switch (e.keyCode) {
+        case 37:
+            move("left");
+            break;
+        case 38:
+            move("up")
+            break;
+        case 39:
+            move("right")
+            break;
+        case 40:
+            move("down")
+            break;
+    }
+
+    console.log(e.keyCode)
+}
+
+// Events
+
+window.addEventListener('click', callElement);
+
+window.addEventListener('keydown', moveElement);
 
 squareDiv.addEventListener('mousedown', (e) => {
     squareDiv.style.backgroundColor = 'rgb(255, 71, 82)';

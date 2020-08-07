906249767b6cbd73c1138b8aa12b399833d7014e
jstrzyzykowski <jakub.strzyzykowski@gmail.com>
2020-08-07 22:04:49 +0200

    Finished

---
+++ AnimalSpinWheel/main.js
@@ -0,0 +1,64 @@
+let $spinBtn;
+let $wheelDiv;
+let $animalsIcons = [...document.querySelectorAll('.animals i')];
+let $ul;
+
+let spinValue;
+let spinAnimalNumber;
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+}
+
+const prepareDOMElements = () => {
+    $spinBtn = document.querySelector('.spin');
+    $wheelDiv = document.querySelector('.wheel');
+    $ul = document.querySelector('.results');
+}
+
+const prepareDOMEvents = () => {
+    $spinBtn.addEventListener('click', spinWheel);
+    $wheelDiv.addEventListener('animationend', finishSpinWheel);
+}
+
+const spinWheel = () => {
+    $animalsIcons.forEach(icon => {
+        icon.classList.remove('active');
+    });
+    spinValue = (5 * 360) + Math.floor(Math.random() * 4) * 90;
+    spinAnimalNumber = (spinValue / 90) % 20;
+
+    document.documentElement.style.setProperty('--spinValue', `${spinValue}deg`);
+    $spinBtn.classList.add('disabled');
+    $wheelDiv.classList.add('on');
+}
+
+const finishSpinWheel = () => {
+    $wheelDiv.style.transform = `rotate(${spinValue}deg)`;
+    $wheelDiv.classList.remove('on');
+    $spinBtn.classList.remove('disabled');
+    $animalsIcons[spinAnimalNumber].classList.add('active');
+
+    // Add new result to list
+    const li = document.createElement('li');
+    switch (spinAnimalNumber) {
+        case 0:
+            li.innerText = 'Hippo';
+            break;
+        case 1:
+            li.innerText = 'Spider';
+            break;
+        case 2:
+            li.innerText = 'Kiwi';
+            break;
+        case 3:
+            li.innerText = 'Horse';
+            break;
+    }
+
+    $ul.appendChild(li);
+}
+
+
+document.addEventListener('DOMContentLoaded', main);

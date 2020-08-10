648f61018bf2fefe3753ff5d06a4fbf1c9d39971
jstrzyzykowski <jakub.strzyzykowski@gmail.com>
2020-08-10 12:39:32 +0200

    Finished

--- SoundPads/main.js
+++ SoundPads/main.js
@@ -1,21 +1,21 @@
 const pads = [...document.querySelectorAll('.pad')];
+const sounds = document.querySelectorAll('.sound');
 
 const check = (e) => {
     const padKey = e.target.closest('.pad').dataset.key;
     const pad = pads[padKey];
 
+    if (!pad.classList.contains('pad-active')) {
+        pad.classList.add('pad-active');
+        pad.style.animation = 'push 2s ease 1 forwards';
+        sounds[padKey].currentTime = 0;
+        sounds[padKey].play();
+    }
+
     pad.addEventListener('animationend', () => {
         pad.classList.remove('pad-active');
         pad.style.animation = '';
     });
-
-    if (!pad.classList.contains('pad-active')) {
-        pad.classList.add('pad-active');
-        pad.style.animation = 'push 2s ease 1 forwards';
-    }
-
-
-    // Wyłączyć animację z PAD -> animation none animation ''
 }
 
 
